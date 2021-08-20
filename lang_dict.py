cad8c88006b4b36a524af266cf573a60e8f54232
Dr0ckman <cmn.kkckdbb@gmail.com>
2021-08-20 08:05:24 -0400

    Add files via upload
    Language codes

---
+++ lang_dict.py
@@ -0,0 +1,15 @@
+lang_dict = {"Alemán": "de_DE",
+             "Checo": "cs_CZ",
+             "Español": "es_MX",
+             "Francés": "fr_FR",
+             "Griego": "el_GR",
+             "Húngaro": "hu_HU",
+             "Inglés": "en_US",
+             "Italiano": "it_IT",
+             "Japonés": "jp_JP",
+             "Polaco": "pl_PL",
+             "Portugués": "pt_BR",
+             "Rumano": "ro_RO",
+             "Ruso": "ru_RU",
+             "Turco": "tr_TR"
+             }
